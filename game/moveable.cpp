a5de1ca251289f1e8e832ad1f1168ff4a7c8c263
tyduszkaszok <126802803+tyduszkaszok@users.noreply.github.com>
2023-05-22 22:11:09 +0200

    Update moveable.cpp

--- game/moveable.cpp
+++ game/moveable.cpp
@@ -1,8 +1,25 @@
-
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 #include "moveable.h"
 
-Moveable::Moveable()
+Moveable::Moveable(const std::vector<int> &position, const std::vector<int> &size)
 {
+    if(position.size()==3)
+    {
+        position_ = position;
+    }
+    else
+    {
+        std::cout << "Enter proper values" << std::endl;
+    }
 
+    if(size.size()==3)
+    {
+        size_ = size;
+    }
+    else
+    {
+        std::cout << "Enter proper values" << std::endl;
+    }
 }
 
