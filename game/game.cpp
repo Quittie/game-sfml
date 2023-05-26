f0b7675d5275110c7793bed2a40f868352bd58dd
tyduszkaszok <126802803+tyduszkaszok@users.noreply.github.com>
2023-05-26 20:43:20 +0200

    lined up

--- game/game.cpp
+++ game/game.cpp
@@ -1,21 +1,17 @@
-
 #include "game.h"
+#include "barricadem.h"
 
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
 #include <iostream>
 #include <vector>
-#include "barricadem.h"
-
-
 
 Game::Game()
 {
     std::cout << "Let's start the game!" << std::endl;
     this->variablesInit();
     this->windowInit();
-
 }
 
 Game::~Game()
@@ -27,56 +23,11 @@ void Game::add(const BarricadeM &object)
     _object.push_back(object);
     std::cout << "Added successfully!" << std::endl;
 }
-//sf::Sprite Game::get(int number)
-//{
-//    return _object[number];
-//}
 
-//void Game::start()
-//{
-//    sf::VideoMode desktopM = sf::VideoMode::getDesktopMode();
-//    sf::RenderWindow window(desktopM, "Soccer Challenge 2k23", sf::Style::Titlebar | sf::Style::Close/* | sf::Style::Fullscreen*/);
-
-//    while (window.isOpen()) {
-//        // check all the window's events that were triggered since the last iteration of the loop
-//        sf::Event event;
-//        while (window.pollEvent(event)) {
-//            // "close requested" event: we close the window
-//            if (event.type == sf::Event::Closed)
-//                window.close();
-////            if (event.key.code == sf::Keyboard::Escape)
-////            {
-////                window.setSize(sf::Vector2u(800, 1200));
-
-////            }
-//        }
-
-
-
-//        // clear the window with black color
-//        window.clear(sf::Color::Black);
-
-////        for(auto &o : _object)
-////        {
-//////            window.draw(o.getRectangleShape());
-//////            o.moveBarricade();
-////        }
-
-
-//        // end the current frame
-//        window.display();
-//    }
-
-//}
-const bool Game::isWindowOpen()
+bool Game::isWindowOpen()
 {
     return this->window->isOpen();
 }
-
-//void Game::stop()
-//{
-
-//}
 
 void Game::updating()
 {
@@ -90,10 +41,10 @@ void Game::updating()
                 this->window->setSize(sf::Vector2u(800, 600));
             }
             break;
+
         case sf::Event::Closed:
             this->window->close();
             break;
-
         }
     }
 }
@@ -101,14 +52,12 @@ void Game::updating()
 void Game::rendering()
 {
     this->window->clear();
-
     this->window->display();
 }
 
 void Game::variablesInit()
 {
     this->window = nullptr;
-
 }
 
 void Game::windowInit()
--- game/game.h
+++ game/game.h
@@ -1,13 +1,14 @@
-
 #ifndef GAME_H
 #define GAME_H
+
+#include "barricadem.h"
 
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
 #include <iostream>
 #include <vector>
-#include "barricadem.h"
+
 
 
 class Game
@@ -15,22 +16,20 @@ class Game
 public:
     Game();
     virtual ~Game();
-//    void start();
-//    void stop();
-    const bool isWindowOpen();
     void add(const BarricadeM &object);
     void updating();
     void rendering();
     BarricadeM get(int number);
 
 private:
-    sf::RenderWindow *window;
-    sf::Event event;
     void variablesInit();
     void windowInit();
-    std::vector<BarricadeM> _object;
+
+    sf::RenderWindow *window;
+    sf::Event event;
     sf::Clock _clock;
     sf::Time _frameTime;
+    std::vector<BarricadeM> _object;
 };
 
 #endif // GAME_H
