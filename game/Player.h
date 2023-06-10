96aacba1f7763d3a9163a6dd07eaf3b3bcdd71ba
Quittie <trusiewicz.piotr03@gmail.com>
2023-06-10 18:24:58 +0200

    player in general

--- game/Player.h
+++ game/Player.h
@@ -1,13 +1,9 @@
-
 #ifndef PLAYER_H
 #define PLAYER_H
 
 #include "moveable.h"
-
-
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
-
 #include <iostream>
 #include <vector>
 #include <string>
@@ -16,17 +12,21 @@
 class Player : public Moveable
 {
 public:
-    Player(const std::vector<float> &position, const std::vector<float> &size, const std::string &source, int velocity);
-    sf::Sprite getSprite();
-    void move();
+    Player(const std::vector<float> &position, const std::vector<float> &size, const std::string &source, float speed);
+    void update(sf::RenderTarget* target);
+    void render(sf::RenderTarget* target);
+    const Sprite &getSprite() const;
 
+    void reset();
 
-
-//    virtual bool checkBounds();
 private:
-    std::string _source;
-    sf::Event ev;
+    sf::Sprite _sprite;
 
+private:
+    void loadTexture(const std::string& source);
+    void loadSprite();
+    void updateInput();
+    void updateWindowBoundsCollision(sf::RenderTarget *target);
 };
 
 #endif // PLAYER_H
--- game/player.cpp
+++ game/player.cpp
@@ -1,103 +1,74 @@
-
 #include "Player.h"
 
 
-Player::Player(const std::vector<float> &position, const std::vector<float> &size, const std::string &source, int velocity):Moveable(position, size, velocity)
+Player::Player(const std::vector<float> &position, const std::vector<float> &size, const std::string &source, float speed): Moveable(position, size, speed)
 {
-    _position = position;
-    _size = size;
+    loadTexture(source);
+    loadSprite();
+} //Player contructor
 
-    if(!this->_texture.loadFromFile(source))
-    {
-        std::cout << "Player not added" << std::endl;
-    }
-    else
-    {
-        std::cout << "Player added" << std::endl;
-    }
-    _sprite.setTexture(_texture);
-    _sprite.setScale(_size[0] / _sprite.getGlobalBounds().width, _size[1] / _sprite.getGlobalBounds().height);
-    _sprite.setPosition(position[0], position[1]);
-    _source = source;
-    _velocity = velocity;
-
-}
-
-
-
-void Player::move()
-{
+void Player::updateInput() {
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
     {
-        this->_sprite.move(-this->_velocity, 0.f);
+        this->_sprite.move(-this->_speed, 0.0f);
     }
     else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
     {
-        this->_sprite.move(this->_velocity, 0.f);
+        this->_sprite.move(this->_speed, 0.0f);
     }
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
     {
-        this->_sprite.move(0.f, -this->_velocity);
+        this->_sprite.move(0.0f, -this->_speed);
     }
-    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
     {
-        this->_sprite.move(0.f, this->_velocity);
-
+        this->_sprite.move(0.0f, this->_speed);
     }
+} // Checks if key was pressend and move in a specified direction
 
+void Player::updateWindowBoundsCollision(sf::RenderTarget *target) {
+    if(this->_sprite.getGlobalBounds().left <= 0.f)
+        this->_sprite.setPosition(0.f,this->_sprite.getGlobalBounds().top);
+
+    if(this->_sprite.getGlobalBounds().left + this ->_sprite.getGlobalBounds().width >= target->getSize().x)
+        this->_sprite.setPosition(target -> getSize().x - this->_sprite.getGlobalBounds().width, this->_sprite.getGlobalBounds().top);
+
+    if(this->_sprite.getGlobalBounds().top <= 0.f)
+        this->_sprite.setPosition(this->_sprite.getGlobalBounds().left,0.f);
+
+    if(this->_sprite.getGlobalBounds().top + this->_sprite.getGlobalBounds().height >= target -> getSize().y)
+        this->_sprite.setPosition(this->_sprite.getGlobalBounds().left, target->getSize().y - this->_sprite.getGlobalBounds().height);
 }
 
-//    sf::Event event;
-//    while (game->getwindow()->pollEvent(event))
-//    {
-//        std::cout << "1" << std::endl;
-//        if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Right)
-//        {
-//            std::cout << "w" << std::endl;
-//            _sprite.setPosition(800, 800);
-//        }
+void Player::update(sf::RenderTarget *target){
+    this->updateInput();
+    this->updateWindowBoundsCollision(target);
+}
 
-//    if(event.type == sf::Event::Closed)
-//    {
-//        game->getwindow()->close();
-//    }
-//    _position[0] += _velocity;
-//    _position[1] += _velocity;
-//    _sprite.setPosition(_position[0], _position[1]);
+void Player::render(sf::RenderTarget* target) {
+    target ->draw(this->_sprite);
+}
 
+void Player::loadTexture(const std::string& source) {
+    if (!this->_texture.loadFromFile(source)) {
+        std::cout << "Player not added" << std::endl;
+    } else {
+        std::cout << "Player added" << std::endl;
+    }
+}
 
-//}
-//void Player::move(Game *game)
-//{
-//    sf::Event event;
-//    while (game->getwindow()->pollEvent(event))
-//    {
-//        if (event.type == sf::Event::KeyPressed)
-//        {
-//            std::cout << "Nienawidze infy" << std::endl;
-//            if (event.key.code == sf::Keyboard::Left)
-//            {
-//                std::cout << "dupa" << std::endl;
-//                _sprite.move(-5, 0);
-//            }
-//            else if (event.key.code == sf::Keyboard::Right)
-//                _sprite.move(5, 0);
-//            else if (event.key.code == sf::Keyboard::Up)
-//                _sprite.move(0, -5);
-//            else if (event.key.code == sf::Keyboard::Down)
-//                _sprite.move(0, 5);
-//        }
-//        else if (event.type == sf::Event::Closed)
-//        {
-//            game->getwindow()->close();
-//        }
-//    }
-//}
+void Player::loadSprite() {
+    _sprite.setTexture(_texture);
+    _sprite.setScale(_size[0] / _sprite.getGlobalBounds().width, _size[1] / _sprite.getGlobalBounds().height);
+    _sprite.setPosition(this->_position[0], this->_position[1]);
+}
 
-sf::Sprite  Player::getSprite()
-{
+const sf::Sprite &Player::getSprite() const {
     return _sprite;
 }
 
-
-//bool Player::checkBounds(){return true;}
+void Player::reset() {
+    _position[0] = _startingPosition[0];
+    _position[1] = _startingPosition[1];
+    _sprite.setPosition(this->_position[0], this->_position[1]);
+}
